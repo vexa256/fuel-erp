b67130da8b4f2101b306c2b9deb6e52cc191b4cb
Ayebare Timothy <vexa256@gmail.com>
2025-06-28 04:39:18 +0300

    v1 initial commit with asset v2 fixt

---
+++ dist/assets/AboutView-Beuo30vk.js
@@ -0,0 +1,1 @@
+import{_ as s,b as t,d as o,i as a}from"./index-BJk99QVQ.js";const n={},r={class:"about"};function c(i,e){return a(),t("div",r,e[0]||(e[0]=[o("h1",null,"This is an about page",-1)]))}const d=s(n,[["render",c]]);export{d as default};
--- dist/assets/AboutView-DYfBy-UT.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,d as t,e as o,p as a}from"./index-Fk_fMhvq.js";const n={},r={class:"about"};function c(_,e){return a(),t("div",r,e[0]||(e[0]=[o("h1",null,"This is an about page",-1)]))}const d=s(n,[["render",c]]);export{d as default};
